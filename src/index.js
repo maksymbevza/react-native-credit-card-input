acf0016a396d58a215063f032e1e5a3d6896eafe
Maksym Bevza <maksymbevza@gmail.com>
2020-12-12 10:21:50 +0200

    add connectToState

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import connectToState, { InjectedProps as IP } from "./connectToState";
+import connectToState as cts, { InjectedProps as IP } from "./connectToState";
 import CCF from "./CreditCardInput";
 import LiteCCF from "./LiteCreditCardInput";
 import CV from "./CardView";
 import CCI from "./CCInput";
 
+export const connectToState = cts;
 export const CreditCardInput = connectToState(CCF);
 export const LiteCreditCardInput = connectToState(LiteCCF);
 export const CardView = CV;
